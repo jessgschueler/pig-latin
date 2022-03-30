fedd5b77ab2ba2844b64cdbe7f3a1cebdf30bc36
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-03-30 16:30:54 -0700

    WIP It will move the first series of consonants of a word that begins with a consonant to the end of the word
    Co-authored-by: Marcus Padilla <marcanthonyzpadilla@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-// Test: "It will add 'w' to the end of words that begin with a vowel."
-// Code: pigLatin("a")
-// Expected Output: "aw"
+// Test: "It will move the first series of consonants of a word that begins with a consonant to the end of the word."
+// Code: pigLatin("space")
+// Expected Output: "acesp"
+
 
 function vowelStartWord(word) {
   let first = word.charAt(0);
@@ -11,3 +12,27 @@ function vowelStartWord(word) {
   return resultVowel
 }
 vowelStartWord("under");
+
+
+  /*  1. take word, turn str to arr, .split(""), divide by char;
+  2. store const conStartWord, in loop: find index of first vowel, .slice(index, removeLtR);
+  3. word.concat(conStartWord + "ay");
+  4. return output outside of local scope. */
+
+//step 1
+// function wordSplit(word) {
+//    return word.split("");
+// }
+// wordSplit("cupcakes");
+// console.log(wordSplit("cupcakes"));
+
+
+//step 2
+// function findFirstVowel(wordArray) {
+
+//   for (let i = 0; i < wordArray.length; i++){
+//     if (wordArray[i] === 'a' || wordArray[i] === "e" || wordArray[i] === "i" || wordArray[i] === "o" || wordArray[i] === "u") {
+//       wordArray.slice(wordArray[i]);
+//     }
+//   }
+// }
