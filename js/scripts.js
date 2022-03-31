36ddeefae61172b1db88be1f9e37d9b91c85d7d4
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-03-31 11:52:40 -0700

    Add function to identify different starting letters.
    Co-authored-by: Mike Wright <m.wright92@icloud.com
    Co-authored-by: Co-authored-by: NIck Benzinger <nicholasbenzinger@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,40 @@
-// Test: "It will move the first series of consonants of a word that begins with a consonant to the end of the word."
-// Code: pigLatin("space")
-// Expected Output: "acesp"
-
+const vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
 function vowelStartWord(word) {
-  let first = word.charAt(0);
-
-    if (first === 'a' || first === "e" || first === "i" || first === "o" || first === "u") {
-    resultVowel = word.concat("w");
+  console.log(word);
+  let resultVowel = [];
+    if (vowel.includes(word[0])) {
+      return true;
+    } else {
+      return false;
     }
-  return resultVowel
 }
-vowelStartWord("under");
 
+function consStartWord(word) {
+  if (!vowel.includes(word[0])) {
+    return true
+  } else {
+    return false
+  }
+}
 
-  /*  1. take word, turn str to arr, .split(""), divide by char;
-  2. store const conStartWord, in loop: find index of first vowel, .slice(index, removeLtR);
-  3. word.concat(conStartWord + "ay");
-  4. return output outside of local scope. */
-
-//step 1
-// function wordSplit(word) {
-//    return word.split("");
-// }
-// wordSplit("cupcakes");
-// console.log(wordSplit("cupcakes"));
-
-
-//step 2
-// function findFirstVowel(wordArray) {
-
-//   for (let i = 0; i < wordArray.length; i++){
-//     if (wordArray[i] === 'a' || wordArray[i] === "e" || wordArray[i] === "i" || wordArray[i] === "o" || wordArray[i] === "u") {
-//       wordArray.slice(wordArray[i]);
-//     }
-//   }
-// }
+function findQu(word) {
+  if (word[0] === "q" && word[1] === "u") {
+    return true;
+  }else {
+    return false;
+  }
+}
+ 
+function pigLatin(text) {
+  let pigArray = text.toLowerCase().split(" ");
+  let outputArray = [];
+  for (i = 0; i < pigArray.length; i++) {
+      let pigVowel = vowelStartWord(pigArray[i]);
+    if (pigVowel === true) {
+      outputArray.push(pigArray + "way");
+    }
+  }
+  // console.log(outputArray);
+  return outputArray;
+}
